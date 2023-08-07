484eeb9649f5a639a746f954d3ebb0697fe7c1c1
Jihye Lee <ehckehck14@gmail.com>
2023-08-07 17:53:08 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/jihye/Login/Login.js
+++ src/pages/jihye/Login/Login.js
@@ -4,33 +4,37 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const navigate = useNavigate();
+  
+  const [ id, setId ] = useState('');
+  const [ pw, setPw ] = useState('');
+  const inputValue = id.includes('@') && pw.length >=5
 
-  const [ id, setId ] = useState();
-  const [ pw, setPw ] = useState();
-
+  console.log(inputValue)
+ 
   const saveUserld = (event) => {
-    setId(event.target.value);
-    console.log(id);
+    setId(event.target.value);  
   }
+  console.log(id);
 
   const saveUserPw = (event) => {
     setPw(event.target.value);
-    console.log(pw);
   }
+  console.log(pw);
 
   return (
     <div className="login">
-      <div id= 'wrapper'>
-        <div id='logo'>Westagram</div>
-        <div id='input'>
-          <input id='id' type = 'text' onChange = { saveUserld } value = { id } placeholder="전화번호, 사용자 이름 또는 이메일"></input>
-          <input id='pw' type = 'password' onChange = { saveUserPw } value = { pw } placeholder="비밀번호"></input>
+      <div className= 'wrapper'>
+        <div className='logo'>Westagram</div>
+        <div className='input'>
+          <input className='id' type = 'text' onChange = { saveUserld } value = { id } placeholder="전화번호, 사용자 이름 또는 이메일"/>
+          <input className='pw' type = 'password' onChange = { saveUserPw } value = { pw } placeholder="비밀번호"/>
         </div>
-        <button id = 'loginBtn' onClick={() => { navigate('/jihye-main');} }>로그인</button>
-        <div id='miss'>비밀번호를 잊으셨나요?</div>
+        <button
+          onClick={() => { navigate("/jihye-main") }} 
+          className= { inputValue ? 'loginBtn buttonLogin' : 'loginBtn'}>로그인</button>
+        <div className='miss'>비밀번호를 잊으셨나요?</div>
       </div>
     </div>
   );
-};
-
+}
 export default Login;
