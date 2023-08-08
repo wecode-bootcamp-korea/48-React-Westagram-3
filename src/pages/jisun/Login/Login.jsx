1402e3f27e992c83273fd9ef08ed5e79c64040ad
JisunHan <hanccino@gmail.com>
2023-08-08 20:00:50 +0900

    로그인 페이지 조건부 렌더링 적용

--- src/pages/jisun/Login/Login.jsx
+++ src/pages/jisun/Login/Login.jsx
@@ -1,35 +1,51 @@
-import React from "react";
-import "./Login.scss";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import react, { useState, useEffect } from "react";
+import "./Login.scss";
 import "../../../style/reset.scss";
-import { getActiveElement } from "@testing-library/user-event/dist/utils";
 
 const Login = () => {
   const navigate = useNavigate();
-  const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
+  //const [userId, setUserId] = useState(""); //string
+  //const [userPw, setUserPw] = useState("");
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
   const [isActive, setIsActive] = useState(false);
 
-  let color = isActive ? "#2d94ef" : "#67b5fa";
+  const [userInfo, setUserInfo] = useState({
+    userId: "",
+    userPw: "",
+  });
 
   useEffect(() => {
-    id.includes("@") && pw.length >= 5 ? setIsActive(true) : setIsActive(false);
-  }, [id, pw]);
+    const condition = userInfo.userId.length >= 5 && userInfo.userId.includes("@");
+    setIsActive(condition);
+  }, [userInfo]);
 
-  const saveUserId = (event) => {
-    setId(event.target.value);
+  console.log(userInfo);
+
+  const handleId = (event) => {
+    //setUserId(event.target.value);
+    const { value } = event.target;
+    setUserInfo({ ...userInfo, userId: value }); // 스프레드 연산자. 특정 대상의 코드를 분해함.
   };
 
-  const saveUserPw = (event) => {
-    setPw(event.target.value);
+  const handlePw = (event) => {
+    //setUserPw(event.target.value);
+    const { value } = event.target;
+    setUserInfo({ ...userInfo, userPw: value });
   };
+
+  const handleInput = (event) => {
+    const { value, id } = event.target;
+    setUserInfo({ ...userInfo, [id]: value }); // [계속해서 변하는 객체 속성명] 대괄호 안에 계속해서 변하는 객체의 속성명 넣어주는 것을 계산된 속성명이라고 한다
+  };
+
+  console.log(userInfo.length);
 
   const goToMain = () => {
-    if (id.includes("@") && pw.length >= 5) {
+    if (isActive) {
       navigate("/jisun-main");
     } else {
-      //document.getElementsByClassName("err").style.display = "block"; 이부분 해결하고싶습니다!
+      setShowErrorMessage(true);
     }
   };
 
@@ -39,29 +55,26 @@ const Login = () => {
         <h1>Westagram</h1>
         <input
           type="text"
-          id="id"
-          value={id}
-          onChange={saveUserId}
+          id="userId"
+          onChange={handleInput}
           placeholder="전화번호, 사용자 이름 또는 이메일"
         />
-        <input type="text" id="pw" value={pw} onChange={saveUserPw} placeholder="비밀번호" />
+        <input type="text" id="userPw" onChange={handleInput} placeholder="비밀번호" />
         <button
           className="btnLogin"
           onClick={goToMain}
-          disabled={!isActive}
-          style={{ backgroundColor: color }}
+          //disabled={!isActive}
+          style={{ backgroundColor: isActive ? "#2d94ef" : "#67b5fa" }}
         >
           로그인
         </button>
         <span>또는</span>
         <button className="btnFacebook">Facebook으로 로그인</button>
-        <p className="err">잘못된 비밀번호입니다. 다시 확인하세요.</p>
-        <a className="findPassword" href="">
-          비밀번호를 잊으셨나요?
-        </a>
+        {showErrorMessage && <p className="err">잘못된 비밀번호입니다. 다시 확인하세요.</p>}
+        <button className="findPassword">비밀번호를 잊으셨나요?</button>
       </div>
       <div className="box">
-        계정이 없으신가요? <a>가입하기</a>
+        계정이 없으신가요? <a href="/signup">가입하기</a>
       </div>
     </div>
   );
