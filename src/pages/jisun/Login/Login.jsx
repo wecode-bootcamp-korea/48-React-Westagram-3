673082ba90f7cd14dc73f2ed279e1b0b7188c61b
JisunHan <hanccino@gmail.com>
2023-08-08 10:03:45 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/jisun/Login/Login.jsx
+++ src/pages/jisun/Login/Login.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 import "./Login.scss";
 import { useNavigate } from "react-router-dom";
-import react, { useState } from "react";
+import react, { useState, useEffect } from "react";
 import "../../../style/reset.scss";
+import { getActiveElement } from "@testing-library/user-event/dist/utils";
 
 const Login = () => {
   const navigate = useNavigate();
-
-  const goToMain = () => {
-    navigate("/jisun-main");
-  };
-
   const [id, setId] = useState("");
   const [pw, setPw] = useState("");
+  const [isActive, setIsActive] = useState(false);
+
+  let color = isActive ? "#2d94ef" : "#67b5fa";
+
+  useEffect(() => {
+    id.includes("@") && pw.length >= 5 ? setIsActive(true) : setIsActive(false);
+  }, [id, pw]);
 
   const saveUserId = (event) => {
     setId(event.target.value);
-    console.log(event);
   };
 
   const saveUserPw = (event) => {
     setPw(event.target.value);
+  };
+
+  const goToMain = () => {
+    if (id.includes("@") && pw.length >= 5) {
+      navigate("/jisun-main");
+    } else {
+      //document.getElementsByClassName("err").style.display = "block"; 이부분 해결하고싶습니다!
+    }
   };
 
   return (
@@ -35,7 +45,12 @@ const Login = () => {
           placeholder="전화번호, 사용자 이름 또는 이메일"
         />
         <input type="text" id="pw" value={pw} onChange={saveUserPw} placeholder="비밀번호" />
-        <button className="btnLogin" onClick={goToMain}>
+        <button
+          className="btnLogin"
+          onClick={goToMain}
+          disabled={!isActive}
+          style={{ backgroundColor: color }}
+        >
           로그인
         </button>
         <span>또는</span>
